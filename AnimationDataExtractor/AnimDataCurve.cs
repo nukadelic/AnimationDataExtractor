d99c9bddeb18d351700c3321180c89317d2c38c3
Nukadelic <6582633+nukadelic@users.noreply.github.com>
2024-08-07 02:03:18 +0300

    include demo

---
+++ AnimationDataExtractor/AnimDataCurve.cs
@@ -0,0 +1,12 @@
+
+namespace AnimDataNS
+{
+    using System.Collections.Generic;
+
+    using UnityEngine;
+
+    public class AnimDataCurve : ScriptableObject
+    {
+        public List<AnimDataTypes.DataCurve> data;
+    }
+}
